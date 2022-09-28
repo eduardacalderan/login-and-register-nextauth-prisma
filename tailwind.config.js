be91b14e728fed856a537cf02d6cca709ff14235
Eduarda Calderan <eduardanicolodicalderan@gmail.com>
2022-09-28 20:40:26 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./layouts/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.slate,
+      green: colors.emerald,
+      purple: colors.violet,
+      blue: colors.blue,
+      yellow: colors.amber,
+      pink: colors.fuchsia,
+      red: colors.red,
+    },
+    extend: {},
+  },
+  plugins: [],
+};
